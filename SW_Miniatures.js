b0f11114e70f8a03cec9e268b29468fb91559061
Andrew Ryan C <andruc@gmail.com>
2020-05-11 00:36:55 -0400

    Merge: Auto Activation
    Squashed commit of the following:
    
    commit ae9bd949fce4e2f2f21d9614baa8dc6b63d03cf9
    Author: Andrew Ryan C <andruc@gmail.com>
    Date:   Mon May 11 00:34:25 2020 -0400
    
        Update help to describe Auto Activation
    
    commit 42ecfa3c5ccb815fc1a0699ff749ba256a175733
    Author: Andrew Ryan C <andruc@gmail.com>
    Date:   Mon May 11 00:24:09 2020 -0400
    
        Activate token upon moving
    
    commit f60ec88a6db8ac90d733bada65def12869037fc4
    Author: Andrew Ryan C <andruc@gmail.com>
    Date:   Mon May 11 00:20:41 2020 -0400
    
        Remove irrelevant 'changing' property from log
    
    commit 6a060e448f52af6fd53ecb3709b789fce4dfbb93
    Author: Andrew Ryan C <andruc@gmail.com>
    Date:   Mon May 11 00:19:42 2020 -0400
    
        Fix handleGraphicChange jsdoc

--- SW_Miniatures.js
+++ SW_Miniatures.js
@@ -9,10 +9,43 @@ const StarWarsMinis = (() => {
 
   const version = "0.1.0";
 
+  const ACTIVATED_MARKER = "padlock";
+  const TEAM_COLOURS = [
+    "red",
+    "blue",
+    "green",
+    "brown",
+    "purple",
+    "pink",
+    "yellow",
+  ];
+
   function whois(playerid) {
     return (getObj("player", playerid) || { get: () => "API" }).get(
       "_displayname"
     );
+  }
+
+  function applySetMarkerToToken(token, status) {
+    let currentMarkers = token.get("statusmarkers").split(",");
+    currentMarkers = _.union(currentMarkers, [].concat(status));
+    token.set("statusmarkers", currentMarkers.join(","));
+  }
+
+  function isInBattle(token) {
+    let statusmarkers = token.get("statusmarkers").split(",");
+    return _.intersection(statusmarkers, TEAM_COLOURS).length > 0;
+  }
+
+  function getTeam(token) {
+    if (!isInBattle(token)) return;
+
+    const word = _.intersection(
+      token.get("statusmarkers").split(","),
+      TEAM_COLOURS
+    ).pop();
+
+    return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
   log(``);
@@ -25,19 +58,51 @@ const StarWarsMinis = (() => {
    * this function gets called on any move so filter actions appropriately
    *
    * token:   Graphic - current state of the graphic moved
-   * prev:    Graphic - previous state before the graphic moved
+   * prev:    Attributes - previous attributes state before the graphic moved
    */
   function handleGraphicChange(token, prev) {
     log("SWM: Token changed");
 
-    let gameInProgress = true;
+    let validMove,
+      gameInProgress = true;
 
-    log({ tokenChanged: token.changed, changing: token.changing });
+    log({ tokenChanged: token.changed });
 
     ////////////////////////////
 
     if (!gameInProgress) return;
 
+    validMove =
+      (token.changed && token.changed.lastmove) ||
+      token.changed.top ||
+      token.changed.left;
+
+    if (validMove) {
+      let ctx;
+      try {
+        ctx = {
+          changeSet: token.changed,
+          team: getTeam(token),
+        };
+      } catch (e) {
+        log("Failed to get context from valid move");
+        log({ token, prev });
+        log(e.stack);
+        return;
+      }
+
+      if (ctx.team) {
+        // activate it
+        applySetMarkerToToken(token, ACTIVATED_MARKER);
+        sendChat(
+          "MinisMod",
+          `<div>` +
+            `${ctx.team ? ctx.team + " token" : "Token"} moved ` +
+            `</div>`,
+          null,
+          { noarchive: true }
+        );
+      }
     }
   }
 
@@ -69,31 +134,25 @@ const StarWarsMinis = (() => {
               case "help":
                 sendChat(
                   "MinisMod",
-                  `/w "${who}" ` + `<div>` + `<div>No help yet</div>` + `</div>`
+                  `/w "${who}" ` +
+                    `<div style="margin-top: 1em;">` +
+                    `<div><strong>Auto Activation:</strong></div>` +
+                    `<div>SWMjs will activate any token that is marked with a colour when it moves</div>` +
+                    `</div>`
                 );
                 break;
               case "test":
                 sendChat(
                   "MinisMod",
                   `/w "${who}" ` +
-                    `<div>` +
-                    `<div>Message Received:</div>` +
+                    `<div style="margin-top: 1em;">` +
+                    `<div><strong>Message Received:</strong></div>` +
                     `<div style="margin: 1em 1em 1em 1em;"><code>${msg_orig.content}</code></div>` +
                     JSON.stringify({ msg: msg_orig, version: version }) +
                     `</div>`
                 );
                 break;
               case "join-team":
-                sendChat(
-                  "MinisMod",
-                  `/w "${who}" ` +
-                    `<div>` +
-                    `<div>A New Challenger Approaches</div>` +
-                    `<div style="margin-top: 1em">Green Team</div>` +
-                    `<div><ul><li>${who}</li></ul></div>` +
-                    `</div>`
-                );
-                break;
               case "new-round":
               default:
                 sendChat(
