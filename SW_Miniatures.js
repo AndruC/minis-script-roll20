0583b37d47b8f04b4c7a362baa3d0806b2f40d7c
Andrew Ryan C <andruc@gmail.com>
2020-05-12 21:51:12 -0400

    Generalize the script
    This commit removes the SW references and trims a lot of the fat. In
    this state it can be shared as an auto-activation script.
    
    One precedent here is that we don't commit a version bump. Instead, the
    version number is updated as soon as it is warranted with the suffix
    ~dev. When a version is stable and tested, it is pinned with its own
    commit that removes the suffix. Commits that pin also get a tag. If we
    go with a release branch, these can be cherry-picked after the fact.

--- SW_Miniatures.js
+++ SW_Miniatures.js
@@ -1,13 +1,15 @@
 /**
- * Andrew's messing around again.
+ * Minatures Auto-Activation Script
+ * Author: AndruC
+ * GitHub: https://github.com/AndruC/minis-script-roll20
  */
 
 ////////////////////////////////////////////////////////////////////////////////
 // Code starts below
-const StarWarsMinis = (() => {
+const MinisAA = (() => {
   "use strict";
 
-  const version = "0.2.0";
+  const version = "0.2.0-dev";
 
   const ACTIVATED_MARKER = "padlock";
   const TEAM_COLOURS = [
@@ -26,100 +28,55 @@ const StarWarsMinis = (() => {
     );
   }
 
-  function applySetMarkerToToken(token, status) {
+  function applyMarkersToToken(token, ...status) {
     let currentMarkers = token.get("statusmarkers").split(",");
     currentMarkers = _.union(currentMarkers, [].concat(status));
     token.set("statusmarkers", currentMarkers.join(","));
   }
 
-  function isInBattle(token) {
+  function getTeam(token) {
     let statusmarkers = token.get("statusmarkers").split(",");
-    return _.intersection(statusmarkers, TEAM_COLOURS).length > 0;
+    let teams = _.intersection(statusmarkers, TEAM_COLOURS);
+    return teams.length ? teams.pop() : null;
   }
 
-  function getTeam(token) {
-    if (!isInBattle(token)) return;
-
-    const word = _.intersection(
-      token.get("statusmarkers").split(","),
-      TEAM_COLOURS
-    ).pop();
-
+  function cap(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
   }
 
-  log(``);
-  log(`-==================-`);
-  log(`-=> SWMjs v${version} <=-`);
-  log(`-==================-`);
-  log(``);
+  log(`->->-> MinisAA v${version} <-<-<-`);
 
-  /**
-   * this function gets called on any move so filter actions appropriately
-   *
-   * token:   Graphic - current state of the graphic moved
-   * prev:    Attributes - previous attributes state before the graphic moved
-   */
-  function handleGraphicChange(token, prev) {
-    log("SWM: Token changed");
-
-    let validMove,
-      gameInProgress = true;
-
-    log({ tokenChanged: token.changed });
-
-    ////////////////////////////
-
-    if (!gameInProgress) return;
+  const handleGraphicChange = (token) => {
+    let validMove;
 
     validMove =
       (token.changed && token.changed.lastmove) ||
       token.changed.top ||
       token.changed.left;
 
-    if (validMove) {
-      let ctx;
-      try {
-        ctx = {
-          changeSet: token.changed,
-          team: getTeam(token),
-        };
-      } catch (e) {
-        log("Failed to get context from valid move");
-        log({ token, prev });
-        log(e.stack);
-        return;
-      }
+    if (!validMove) return;
 
-      if (ctx.team) {
-        // activate it
-        applySetMarkerToToken(token, ACTIVATED_MARKER);
-        sendChat(
-          "MinisMod",
-          `<div>` +
-            `${ctx.team ? ctx.team + " token" : "Token"} moved ` +
-            `</div>`,
-          null,
-          { noarchive: true }
-        );
-      }
-    }
-  }
+    const team = getTeam(token);
 
-  function handleChatMessage(msg_orig) {
+    if (!team) return;
+
+    // activate it
+    applyMarkersToToken(token, ACTIVATED_MARKER);
+
+    sendChat("MinisAA", `${cap(team) + " token"} moved. `);
+  };
+
+  const handleCommandLine = (msg_orig) => {
     let args, cmds, who;
+    const { content } = msg_orig;
 
     try {
-      if (msg_orig.type !== "api") {
-        return;
-      }
-
-      const content = msg_orig.content;
+      if (msg_orig.type !== "api") return;
 
       args = content.split(/\s+--/);
 
       switch (args.shift()) {
-        case "!sw-minis": {
+        case "!minis": {
           who = whois(msg_orig.playerid);
 
           while (args.length) {
@@ -127,28 +84,14 @@ const StarWarsMinis = (() => {
               .shift()
               .match(/([^\s]+[|#]'[^']+'|[^\s]+[|#]"[^"]+"|[^\s]+)/g);
 
-            log("SWM: Handling chat input");
-            log({ who, cmds });
-
             switch (cmds.shift()) {
               case "help":
                 sendChat(
-                  "MinisMod",
+                  "MinisAA",
                   `/w "${who}" ` +
                     `<div style="margin-top: 1em;">` +
                     `<div><strong>Auto Activation:</strong></div>` +
-                    `<div>SWMjs will activate any token that is marked with a colour when it moves</div>` +
-                    `</div>`
-                );
-                break;
-              case "test":
-                sendChat(
-                  "MinisMod",
-                  `/w "${who}" ` +
-                    `<div style="margin-top: 1em;">` +
-                    `<div><strong>Message Received:</strong></div>` +
-                    `<div style="margin: 1em 1em 1em 1em;"><code>${msg_orig.content}</code></div>` +
-                    JSON.stringify({ msg: msg_orig, version: version }) +
+                    `<div>MinisAA will activate any token that is marked with a colour when it moves</div>` +
                     `</div>`
                 );
                 break;
@@ -156,12 +99,12 @@ const StarWarsMinis = (() => {
               case "new-round":
               default:
                 sendChat(
-                  "MinisMod",
+                  "MinisAA",
                   `/w "${who}" ` +
                     `<div>` +
                     `<div style="margin-top: 1em;"><strong>Unrecognized command:<strong></div>` +
-                    `<div style="margin: 0.1em 1em 1em 1em;"><code>${msg_orig.content}</code></div>` +
-                    `</div>`
+                    `<div style="margin: 4px 12px 12px;"><code>${content}</code></div>` +
+                    `<div>See <code>!minis --help</code></div>``</div>`
                 );
                 break;
             }
@@ -171,23 +114,20 @@ const StarWarsMinis = (() => {
     } catch (e) {
       who = whois(msg_orig.playerid);
       sendChat(
-        "MinisMod" + version,
+        "MinisAA" + version,
         `/w "${who}" ` +
           `<div>` +
           `<div>There was an error while trying to run your command:</div>` +
-          `<div style="margin: .1em 1em 1em 1em;"><code>${msg_orig.content}</code></div>` +
+          `<div style="margin: 4px 12px 12px;"><code>${content}</code></div>` +
           JSON.stringify({ msg: msg_orig, version: version, stack: e.stack }) +
           `</div>`
       );
     }
-  }
+  };
 
   const registerEventHandlers = function () {
-    on("chat:message", handleChatMessage);
+    on("chat:message", handleCommandLine);
     on("change:graphic", handleGraphicChange);
-    if (TokenMod) {
-      TokenMod.ObserveTokenChange(handleGraphicChange);
-    }
   };
 
   on("ready", function () {
