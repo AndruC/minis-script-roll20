6960d292a24f2c4547fe93af678ba278c77ca8ff
Andrew Cousineau <AndruC@users.noreply.github.com>
2020-05-16 17:40:22 -0400

    Merge: Join Team
    This change removes the test command entirely, and reduces the set of
    possible teams down to just red, green, blue, and yellow. 
    
    It adds the "--join-team" command which exclusively adds a coloured badge
    to selected tokens. Because teams are tied to Auto Activation, the command
    also resets the activation status by removing the associated badge.

--- auto-activation.js
+++ auto-activation.js
@@ -11,16 +11,41 @@ const MinisAA = (() => {
 
   const version = "0.2.0-dev";
 
-  const ACTIVATED_MARKER = "padlock";
-  const TEAM_COLOURS = [
-    "red",
-    "blue",
-    "green",
-    "brown",
-    "purple",
-    "pink",
-    "yellow",
-  ];
+  // Feel free to expand this list as needed. Requires a corresponding badge.
+  const TEAM_COLOURS = ["red", "blue", "green", "yellow"];
+
+  // Similarly this can be changed to the name of any available badge
+  const ACTIVATED_BADGE = "padlock";
+
+  const Movement = (token) => {
+    const hasChanged = !!token.changed;
+    if (!hasChanged) return false;
+
+    const hasMoved = !!token.changed.lastmove;
+    const lastMoves = hasMoved ? token.get("lastmove").split(",") : [];
+
+    let currentLeft, currentTop, oldLeft, oldTop;
+
+    currentLeft = token.get("left");
+    currentTop = token.get("top");
+
+    // lastMoves should always have a multiple of 2 elements, zero-inclusive
+    const steps = lastMoves.length % 2 ? null : lastMoves.length / 2;
+
+    oldLeft = lastMoves.length >= 2 ? Number(lastMoves.slice(0, 1)) : null;
+    oldTop = lastMoves.length >= 2 ? Number(lastMoves.slice(1, 2)) : null;
+
+    // catch undos (or the very first move on the map) [bug]
+    if (oldLeft === null && oldTop === null) return false;
+
+    // catch new tokens
+    if (oldLeft === currentLeft && oldTop === currentTop) return false;
+
+    const position = [currentLeft, currentTop];
+    const from = [].concat(oldLeft, oldTop);
+
+    return { hasMoved, position, from, steps };
+  };
 
   function whois(playerid) {
     return (getObj("player", playerid) || { get: () => "API" }).get(
@@ -47,31 +72,27 @@ const MinisAA = (() => {
   log(`->->-> MinisAA v${version} <-<-<-`);
 
   const handleGraphicChange = (token) => {
-    let validMove;
-
-    validMove =
-      (token.changed && token.changed.lastmove) ||
-      token.changed.top ||
-      token.changed.left;
-
-    if (!validMove) return;
-
     const team = getTeam(token);
-
     if (!team) return;
 
+    const move = Movement(token);
+    if (!move) return;
+
     // activate it
-    applyMarkersToToken(token, ACTIVATED_MARKER);
+    applyMarkersToToken(token, ACTIVATED_BADGE);
 
     sendChat("MinisAA", `${cap(team) + " token"} moved. `);
   };
 
   const handleCommandLine = (msg_orig) => {
+    // rename to handleCommandLine ?
     let args, cmds, who;
     const { content } = msg_orig;
 
     try {
       if (msg_orig.type !== "api") return;
+
+      const selected = msg_orig.selected;
 
       args = content.split(/\s+--/);
 
@@ -90,20 +111,103 @@ const MinisAA = (() => {
                   "MinisAA",
                   `/w "${who}" ` +
                     `<div style="margin-top: 1em;">` +
+                    `<div><strong>Join Team:</strong></div>` +
+                    `<code>!minis --join-team</code>` +
+                    `<div style="margin-top: 1em;">` +
+                    `Add the selected tokens to a team. A token can only ` +
+                    `be added to one team at a time. Adding a token to a ` +
+                    `team will reset its activation status if it has one.` +
+                    `</div>` +
+                    `<div style="margin-top: 1em;">` +
+                    `<div>Valid teams are:</div>` +
+                    `<ul>` +
+                    TEAM_COLOURS.map((c) => `<li>${c}</li>`).join(" ") +
+                    `</ul>` +
+                    `</div>` +
+                    `<div style="margin-top: 1em;">` +
                     `<div><strong>Auto Activation:</strong></div>` +
-                    `<div>MinisAA will activate any token that is marked with a colour when it moves</div>` +
+                    `<div style="margin-top: 1em;">` +
+                    `Once a token is on a team it will be activated ` +
+                    `whenever that token is dropped in a new tile. The ` +
+                    `activation status is indicated by the padlock badge ` +
+                    `but this can be changed in script by modifying the ` +
+                    `ACTIVATED_MARKER value.` +
+                    `</div>` +
                     `</div>`
                 );
                 break;
               case "join-team":
+                if (!selected) {
+                  sendChat(
+                    "MinisMod",
+                    `/w "${who}" ` +
+                      `<div style="margin-top: 1em;">` +
+                      `<div><strong>ERROR: No tokens selected</strong></div>` +
+                      `<div>See <code>!minis --help join-team</code></div>` +
+                      `</div>`
+                  );
+                  return;
+                }
+
+                if (cmds.length !== 1) {
+                  sendChat(
+                    "MinisMod",
+                    `/w "${who}" ` +
+                      `<div style="margin-top: 1em;">` +
+                      `<div><strong>ERROR: No team chosen</strong></div>` +
+                      `<div>See <code>!minis --help join-team</code></div>` +
+                      `</div>`
+                  );
+                  return;
+                }
+
+                if (!_.contains(TEAM_COLOURS, cmds[0])) {
+                  sendChat(
+                    "MinisMod",
+                    `/w "${who}" ` +
+                      `<div style="margin-top: 1em;">` +
+                      `<div><strong>ERROR: No valid team provided</strong></div>` +
+                      `<div>See <code>!minis --help join-team</code></div>` +
+                      `</div>`
+                  );
+                  return;
+                }
+
+                const team = cmds[0];
+
+                selected
+                  .map((sel) => getObj("graphic", sel._id))
+                  .map((token) => ({
+                    token,
+                    markers: token.get("statusmarkers").split(","),
+                  }))
+                  .map(({ token, markers }) => ({
+                    token,
+                    markers: _.chain(markers)
+                      // remove all team markers and deactivate badges
+                      // then add the new team badge
+                      .difference(TEAM_COLOURS.concat(ACTIVATED_BADGE))
+                      .union([team])
+                      .value(),
+                  }))
+                  .forEach(({ token, markers }) => {
+                    token.set("statusmarkers", markers.join(","));
+                  });
+
+                sendChat(
+                  "MinisMod",
+                  `/w "${who}" ` +
+                    `<div>Added ${selected.length} to ${cmds[0]} team</div>`
+                );
+                break;
               case "new-round":
               default:
                 sendChat(
                   "MinisAA",
                   `/w "${who}" ` +
-                    `<div>` +
-                    `<div style="margin-top: 1em;"><strong>Unrecognized command:<strong></div>` +
-                    `<div style="margin: 4px 12px 12px;"><code>${content}</code></div>` +
+                    `<div style="margin-top: 1em;">` +
+                    `<div><strong>ERROR: Unrecognized command:</strong></div>` +
+                    `<code>${content}</code>` +
                     `<div>See <code>!minis --help</code></div>` +
                     `</div>`
                 );
